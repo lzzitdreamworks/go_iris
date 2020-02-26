9c7daf66d4f46cc203e677015e328a14a8df2e25
it_1024 <it_1024@163.com>
2020-02-26 17:10:08 +0800

    iris post get 请求测试

--- main.go
+++ main.go
@@ -7,6 +7,12 @@ import (
 	"github.com/kataras/iris/v12/middleware/recover"
 )
 
+type Person struct {
+	Username string `json:"username"`
+	Pwd string `json:"pwd"`
+}
+
+
 func main() {
 	app := iris.New()
 	app.Logger().SetLevel("debug")
@@ -19,20 +25,51 @@ func main() {
 	// Method:   GET
 	// Resource: http://localhost:8080
 	app.Handle("GET", "/", func(ctx iris.Context) {
-		ctx.HTML("<h1>Welcome</h1>")
+		ctx.HTML("<h1>Go Web Iris, Welcome</h1>")
 	})
 
 	// same as app.Handle("GET", "/ping", [...])
 	// Method:   GET
 	// Resource: http://localhost:8080/ping
 	app.Get("/ping", func(ctx iris.Context) {
-		ctx.WriteString("pong")
+		path := ctx.Path
+		app.Logger().Info(path)
+		username := ctx.URLParam("username")
+		app.Logger().Info(username)
+		password := ctx.URLParam("pwd")
+		app.Logger().Info(password)
+	//	ctx.WriteString(string(path))
+		ctx.HTML("<H1>" + username + "," + password + "</h1>")
 	})
 
 	// Method:   GET
 	// Resource: http://localhost:8080/hello
 	app.Get("/hello", func(ctx iris.Context) {
 		ctx.JSON(iris.Map{"message": "Hello Iris!"})
+	})
+
+
+	app.Post("/login", func(ctx iris.Context) {
+		path := ctx.Path
+		app.Logger().Info(path)
+		username := ctx.PostValue("username")
+		app.Logger().Info(username)
+		password := ctx.PostValue("pwd")
+		app.Logger().Info(password)
+	//	ctx.WriteString(string(path))
+		ctx.HTML("<H1>" + username + "," + password + "</h1>")
+	})
+
+	app.Post("/postjson", func(ctx iris.Context) {
+		path := ctx.Path
+		app.Logger().Info(path)
+		var person Person
+		if err := ctx.ReadJSON(&person); err != nil {
+			panic(err.Error())
+		}
+		
+	//	ctx.WriteString(string(path))
+		ctx.Writef("Received", person)
 	})
 
 	// http://localhost:8080
