5cc2c2b0bebc16e11cd449fd49a1d23dc75a8246
it_1024 <it_1024@163.com>
2020-02-29 17:51:36 +0800

    iris测试连接到MySQL数据库

--- main.go
+++ main.go
@@ -1,17 +1,50 @@
 package main
 
 import (
+	"fmt"
+	"github.com/go-xorm/xorm"
 	"github.com/kataras/iris/v12"
-
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/kataras/iris/v12/middleware/logger"
 	"github.com/kataras/iris/v12/middleware/recover"
+	"log"
 )
+
+const DriverName = "mysql"
+// "数据库账号:数据库密码@tcp(数据库Ip:端口)/数据库名称?parseTime=true"
+const MasterDataSourceName = "root:@tcp(127.0.0.1:3306)/otott_slave?charset=utf8"
+
+var engine *xorm.Engine
+
+func execute () {
+    sql := "INSERT INTO `admin`(`id`, `username`, `password`, `true_name`, `email`, `mobile`, `remark`, `create_admin_id`, `last_login_ip`, `last_login_time`, `login_count`, `last_logout_time`, `create_time`, `update_time`, `status`) VALUES (29, 'sz_admin', '$2y$13$nHT706UX/3qDO2FunpzcquY0YxLDw3X9buoXoVTITA3dLXT1bzFxq', '深圳admin', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-12-20 14:48:49', '2019-12-20 14:48:49', 1);"
+    affected, err := engine.Exec(sql)
+    if err != nil {
+        log.Fatal("execute error", err)
+
+    } else {
+        id, _ := affected.LastInsertId()
+        rows, _ := affected.RowsAffected()
+        fmt.Println("execute id=", id, ",rows=", rows)
+    }
+}
+
+// 连接到数据库
+func newEngin() *xorm.Engine {
+	engine, err := xorm.NewEngine(DriverName, MasterDataSourceName)
+	if err != nil {
+		log.Fatal(newEngin, err)
+		return nil
+	}
+	// Debug模式，打印全部SQL语句，帮助对比
+	engine.ShowSQL(true)
+	return engine
+}
 
 type Person struct {
 	Username string `json:"username"`
 	Pwd string `json:"pwd"`
 }
-
 
 func main() {
 	app := iris.New()
@@ -21,6 +54,9 @@ func main() {
 	// and log the requests to the terminal.
 	app.Use(recover.New())
 	app.Use(logger.New())
+
+	engine = newEngin()
+	execute() // 测试插入一条语句
 
 	// Method:   GET
 	// Resource: http://localhost:8080
